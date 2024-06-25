8911bbbffec93a9a1b709c196fdfe4c87fe9d3a5
jose-alberto-hurtado <josealberto1829@gmail.com>
2024-06-24 21:12:19 -0600

    version2

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-import pandas as pd
-import plotly.express as px
+import streamlit as st
 
-car_data = pd.read_csv('vehicles_us.csv') # leer los datos
-fig = px.histogram(car_data, x="odometer") # crear un histograma
-fig.show() # crear gráfico de dispersión#app.py
+# crear una casilla de verificación
+build_histogram = st.checkbox('Construir un histograma')
+
+if build_histogram: # si la casilla de verificación está seleccionada
+    st.write('Construir un histograma para la columna odómetro')
